f022f61ac733a74ae85c8050ef303ff6046d31fc
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-02-28 16:16:03 +0200

    add internal hashes

--- contracts/RefundProvider.sol
+++ contracts/RefundProvider.sol
@@ -146,15 +146,15 @@ contract RefundProvider is RefundState, IERC721Receiver {
     }
 
     ///@dev collateral receives user refund amount if the time has not expired
-    function _handleCollateralWithdraw(uint256 poolId, uint256 amount) internal {
+    function _handleCollateralWithdraw(uint256 poolId, uint256 amount) internal firewallProtectedSig(0xf6071d4c) {
         if (!collateralProvider.isPoolFinished(poolIdToCollateralId[poolId])) {
             collateralProvider.handleWithdraw(poolIdToCollateralId[poolId], amount);
         }
     }
 
     ///@dev transfer data NFT (poolId + 1) to the pool owner
-    function _transferDataNFT(uint256 poolId, uint256 userDataPoolId, uint256 fullAmount, uint256 amountToBeWithdrawn) internal {
-        if (fullAmount > amountToBeWithdrawn) {
+    function _transferDataNFT(uint256 poolId, uint256 userDataPoolId, uint256 amount, uint256 amountToBeWithdrawn) internal firewallProtectedSig(0x3ef1dad7) {
+        if (amount > amountToBeWithdrawn) {
             lockDealNFT.safeTransferFrom(address(this), lastPoolOwner[poolId], userDataPoolId);
         }
     }
